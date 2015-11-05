6a4b2051d6c429848a0c0a678cb0a7f9806389e5
zyko <matthias.burkhardt@ymail.com>
2015-11-05 11:51:28 +0100

    added project PoKeIt

---
+++ PoKeIt/Intermediate/Build/Win64/UE4Editor/Development/PoKeIt/PCH.PoKeIt.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\GitHub\PoKeIt\PoKeIt\Source\PoKeIt\PoKeIt.h"
---
+++ PoKeIt/Source/PoKeIt/PoKeItGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PoKeItGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POKEIT_API APoKeItGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
