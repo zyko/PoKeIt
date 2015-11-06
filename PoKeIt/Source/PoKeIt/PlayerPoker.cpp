27e2eea11b2dee59cf36b88d41f09338d1b581bb
zyko <matthias.burkhardt@ymail.com>
2015-11-06 16:27:56 +0100

    added PlayerPoker.cpp

---
+++ PoKeIt/Source/PoKeIt/PlayerPoker.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PoKeIt.h"
+#include "PlayerPoker.h"
+
+
+public int pokerChips;
+
+
+
+PlayerPoker::PlayerPoker()
+{
+}
+
+PlayerPoker::~PlayerPoker()
+{
+}
+
+
+
+PlayerPoker::public void receiveCard()
+{
+
+}
+
+
+#pragma region actions
+
+
+PlayerPoker::public void call()
+{
+
+}
+
+PlayerPoker::public void check()
+{
+
+}
+
+PlayerPoker::public void raise()
+{
+
+}
+
+PlayerPoker::public void fold()
+{
+
+}
+
+#pragma endregion
